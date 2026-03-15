bed5f88666e93937c5ab33c11256849adc5e1b4f
victory-aime <victorydarnelmbenze@gmail.com>
2026-03-15 15:25:23 +0100

    feat(init bo app): init bo app

---
+++ next.config.ts
@@ -0,0 +1,45 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  turbopack: {},
+  /* use redirect proxy for api calls
+   * every request to /api/* will be redirected to the backend server
+   * */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.API_BACKEND_URL}/api/:path*`,
+      },
+      {
+        source: "/api/v1/:path*",
+        destination: `${process.env.API_BACKEND_URL}/api/v1/:path*`,
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "platform-lookaside.fbsbx.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatar.iran.liara.run",
+        pathname: "/**",
+      },
+    ],
+  },
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
---
+++ src/app/routes/routes.ts
@@ -0,0 +1,1 @@
+export const BO_ROUTES = {};
